160ad524b0fc481ef1293081f82abc396d403fe1
Samuel Boateng <kaybeta500@gmail.com>
2019-10-18 14:29:21 +0000

    more security fixes

--- backend/views.py
+++ backend/views.py
@@ -4,4 +4,4 @@ from django.template.loader import get_template
 from django.conf import settings
 
 template_name = 'index.html' if settings.DEBUG == True else get_template('index.html')
-index = never_cache(TemplateView.as_view(template_name=get_template('index.html')))
+index = never_cache(TemplateView.as_view(template_name=template_name))
