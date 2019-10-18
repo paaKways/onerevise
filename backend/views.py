08c7b938bbf107a7a1c20850e21295f7ef2e6016
Samuel Boateng <kaybeta500@gmail.com>
2019-10-18 13:25:53 +0000

    changes

--- backend/views.py
+++ backend/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView
 from django.views.decorators.cache import never_cache
+from django.template.loader import get_template
 
-index = never_cache(TemplateView.as_view(template_name='index.html'))
+index = never_cache(TemplateView.as_view(template_name=get_template('index.html')))
